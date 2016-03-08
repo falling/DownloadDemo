3292b159a0509e23deacda6eb6a7f67f1df53f57
falling <zyz635857938@gmail.com>
2016-03-08 15:36:40 +0800

    重构代码

--- app/src/main/java/com/example/falling/downloaddemo/DownloadActivity.java
+++ app/src/main/java/com/example/falling/downloaddemo/DownloadActivity.java
@@ -3,29 +3,30 @@ package com.example.falling.downloaddemo;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class DownloadActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final int INVISIBLE = -2;
+    public static final int NOCHANGE = -1;
     private Button mDownloadButton;
     private EditText mUrlText;
     private String mUrl;
     private ProgressBar mProgressBar;
+    private String mButton_Text;
+    private String mToast_Text;
     private int mProgress;
 
     @Override
@@ -42,7 +43,7 @@ public class DownloadActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.download_button:
-                if(InternetUtil.isNetworkConnected(v.getContext())) {
+                if (InternetUtil.isNetworkConnected(v.getContext())) {
                     mUrl = mUrlText.getText().toString();
                     mProgressBar.setVisibility(View.VISIBLE);
                     new Thread(new Runnable() {
@@ -51,82 +52,117 @@ public class DownloadActivity extends AppCompatActivity implements View.OnClickL
                             downloadInBackGround(mUrl);
                         }
                     }).start();
-                }else{
-                    Toast.makeText(v.getContext(),"无法上网！",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(v.getContext(), "无法上网！", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
     }
 
     private void downloadInBackGround(String strUrl) {
+        int TotalSize; //总文件大小
+        String downloadFolderName; //下载的目录文件夹
+        String downloadFileName;    //下载的文件名
+        InputStream inputStream; //下载的流
         try {
             URL url = new URL(strUrl);
             URLConnection urlConnection = url.openConnection();
-            InputStream inputStream = urlConnection.getInputStream();
-            int TotalSize = urlConnection.getContentLength();
-            String downloadFolderName = Environment.getExternalStorageDirectory() + "/MyDownloadDemo/";
-            Log.i("Download",downloadFolderName);
-            File file = new File(downloadFolderName);
-            if (!file.exists()) {
-                if (!file.mkdir()) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(DownloadActivity.this, "创建文件失败，是否有读写权限？", Toast.LENGTH_SHORT).show();
-                            mProgressBar.setVisibility(View.INVISIBLE);
-                        }
-                    });
-                    return;
-                }
-            }
+            inputStream = urlConnection.getInputStream();
+            TotalSize = urlConnection.getContentLength();
 
+            //文件路径
+            downloadFolderName = Environment.getExternalStorageDirectory() + "/MyDownloadDemo/";
+            File foldName = new File(downloadFolderName);
+            //文件名
             String strings[] = strUrl.split("/");
-            File apkFile = new File(downloadFolderName + strings[strings.length - 1]);
-            Log.i("Download",downloadFolderName + strings[strings.length - 1]);
-            if (apkFile.exists()) {
-                if (!apkFile.delete()) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(DownloadActivity.this, "删除文件失败，是否有读写权限？", Toast.LENGTH_SHORT).show();
-                            mProgressBar.setVisibility(View.INVISIBLE);
-                        }
-                    });
-                    return;
-                }
-            }
+            downloadFileName = downloadFolderName + strings[strings.length - 1];
+            File fileName = new File(downloadFileName);
 
-            int downloadSize = 0;
-            byte[] bytes = new byte[1024];
-            int length = 0;
-            OutputStream outputStream = new FileOutputStream(apkFile);
-            while ((length = inputStream.read(bytes)) != -1) {
-                outputStream.write(bytes, 0, length);
-                outputStream.flush();
-                downloadSize += length;
-                mProgress = downloadSize *100 / TotalSize;
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mProgressBar.setProgress(mProgress);
-                        mDownloadButton.setText("下载中");
+            if (checkFoldAndFile(foldName, fileName)) return;
 
-                    }
-                });
-            }
-
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mDownloadButton.setText("下载");
-                    Toast.makeText(DownloadActivity.this,"下载完成",Toast.LENGTH_SHORT).show();
-
-                }
-            });
-
+            startDownload(TotalSize, inputStream, fileName);
+            myRunOniThread("下载", NOCHANGE, "下载成功");
 
         } catch (IOException e) {
             e.printStackTrace();
+            myRunOniThread(null, INVISIBLE, "创建文件夹或删除文件失败，是否有读写权限？");
         }
+    }
+
+    /**
+     * 检查文件夹是否创建，未创建则创建。 检查要下载的文件是否已经存在，存在则删除。
+     * @param foldName
+     * @param fileName
+     * @return
+     */
+    private boolean checkFoldAndFile(File foldName, File fileName) {
+        if (!foldName.exists()) {
+            if (!foldName.mkdir()) {
+                myRunOniThread(null, INVISIBLE, "创建文件夹失败，是否有读写权限？");
+                return true;
+            }
+        }
+
+        if (fileName.exists()) {
+            if (!fileName.delete()) {
+                myRunOniThread(null, INVISIBLE, "删除文件失败，是否有读写权限？");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 开始下载文件
+     * @param totalSize 总文件大小
+     * @param inputStream 下载的输入流
+     * @param fileName  文件名
+     * @throws IOException
+     */
+    private void startDownload(int totalSize, InputStream inputStream, File fileName) throws IOException {
+        int downloadSize = 0;
+        byte[] bytes = new byte[1024];
+        int length;
+        OutputStream outputStream = new FileOutputStream(fileName);
+
+        while ((length = inputStream.read(bytes)) != NOCHANGE) {
+            outputStream.write(bytes, 0, length);
+            outputStream.flush();
+            downloadSize += length;
+            int progress = downloadSize * 100 / totalSize;
+            myRunOniThread("下载中", progress, null);
+
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+
+    /**
+     * @param button_text 更改按钮文字,不更改则输入 null；
+     * @param progress    更改进度条的进度显示，不更改则输入 NOCHANGE；隐藏则 输入 INVISIBLE；
+     * @param toast_text  toast显示的文字 不更改则输入 null
+     */
+    private void myRunOniThread(String button_text, int progress, String toast_text) {
+        mButton_Text = button_text;
+        mProgress = progress;
+        mToast_Text = toast_text;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mButton_Text != null) {
+                    mDownloadButton.setText(mButton_Text);
+                }
+                if (mProgress >= 0) {
+                    mProgressBar.setProgress(mProgress);
+                } else if (mProgress == INVISIBLE) {
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                }
+                if (mToast_Text != null) {
+                    Toast.makeText(DownloadActivity.this, mToast_Text, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
